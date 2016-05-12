90b9e29efa6539907a19ce2780f68dadf8e01506
brianjly <brian.ly12@gmail.com>
2016-05-12 12:42:56 -0700

    users model

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 var Data = require('../models/data');
+var User = require('../models/users')
 
 Data.methods(['get','put', 'post', 'delete']);
 Data.register(router, '/data');
+
+User.methods(['get','put', 'post', 'delete']);
+User.register(router, '/users');
+
 
 module.exports = router;
